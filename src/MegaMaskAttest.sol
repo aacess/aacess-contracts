8bd5514db4c44cf2d5a5d878f25d9a6c10db07c9
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-10-22 15:24:16 +0800

    feat: rm hyperlane calls + include wormhole functions

--- src/MegaMaskAttest.sol
+++ src/MegaMaskAttest.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
 //------------------------
 //IMPORT EXTERNAL PACKAGES
 //------------------------
-import "wormhole-solidity-sdk/interfaces/IWormholeRelayer.sol";
-import "wormhole-solidity-sdk/interfaces/IWormholeReceiver.sol";
+import "aacess-contracts/lib/wormhole-solidity-sdk/src/interfaces/IWormholeRelayer.sol";
+import "aacess-contracts/lib/wormhole-solidity-sdk/src/interfaces/IWormholeReceiver.sol";
 
 interface IEAS {
     /// @notice A struct representing the arguments of the attestation request.
@@ -29,23 +29,6 @@ interface IEAS {
     ) external payable returns (bytes32);
 }
 
-// SPDX-License-Identifier: Apache 2
-
-pragma solidity ^0.8.0;
-
-/**
- * @notice Interface for a contract which can receive Wormhole messages.
- */
-interface IWormholeReceiver {
-    function receiveWormholeMessages(
-        bytes memory payload,
-        bytes[] memory additionalVaas,
-        bytes32 sourceAddress,
-        uint16 sourceChain,
-        bytes32 deliveryHash
-    ) external payable;
-}
-
 /**
  * @title MegaMask
  * @dev A smart contract for managing product inventory and attestation on different chains.
@@ -59,7 +42,11 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
     uint256 constant GAS_LIMIT = 50_000;
 
     IEAS public eas;
-    IWormholeRelayer public immutable wormholeRelayer;
+    IWormholeRelayer public wormholeRelayer;
+
+    mapping(bytes32 => bool) public seenDeliveryVaaHashes;
+
+    string public latestAttestation;
 
     //-----------------------------
     //DEFINE EVENTS
@@ -74,22 +61,15 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
         bool revocable,
         bytes32 indexed refUID,
         bytes indexed data,
-        uint256 value
+        uint256 value,
+        bytes32 attestationHash
     );
 
-    /**
-     * @dev Event triggered when the interchain gas paymaster address is updated.
-     * @param newInterchainGasPaymasterAddress The new interchain gas paymaster address.
-     */
-    event InterchainGasPaymasterUpdated(
-        address newInterchainGasPaymasterAddress
+    event AttestationReceived(
+        string greeting,
+        uint16 sourceChain,
+        address sender
     );
-
-    /**
-     * @dev Event triggered when the mailbox address is updated.
-     * @param newMailboxAddress The new mailbox address.
-     */
-    event MailboxUpdated(address newMailboxAddress);
 
     //-----------------------------
     //DEFINE MODIFIERS
@@ -104,20 +84,21 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
         _updateInstances(); // update the instances upon deployment
     }
 
-    //-----------------------------
-    //DEFINE SETTER FUNCTIONS
-    //TODO: function to post product details to different chains at once (hyperlane) DONE
-    //TODO: function to fetch product details from current chain, getProduct(address) -> Product[] (hyperlane) DONE
-    //TODO: post attestation as merchant to attestation contract in Sepolia (wormhole)
-    //TODO: resolver contract to check that a merchant attestaion exists in Sepolia
-    //TODO: function to make an attestation that bill paid (wormhole)
-    //TODO: function to fetch attestation from sepolia (hyperlane)
-    //-----------------------------
+    //function to change the wormhole relayer address
+    function changeWormholeRelayerAddress(
+        address _wormholeRelayerAddress
+    ) external {
+        wormholeRelayerAddress = _wormholeRelayerAddress;
+        _updateInstances();
+    }
 
     //setter function to add product to inventory
     function attest(
         AttestationRequest memory request
-    ) external payable override returns (bytes32) {
+    ) public payable override returns (bytes32) {
+        // Post the attestation to Sepolia
+        bytes32 attestationHash = eas.attest(request);
+
         emit AttestationPostedToSepolia(
             msg.sender,
             request.schema,
@@ -126,10 +107,28 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
             request.data.revocable,
             request.data.refUID,
             request.data.data,
-            request.data.value
+            request.data.value,
+            attestationHash
         );
 
-        return eas.attest(request);
+        return attestationHash;
+    }
+
+    //another attest function but accept a calldata, decode it and call the above function
+    function attestCalldata(
+        bytes calldata request
+    ) external payable returns (bytes32) {
+        bytes memory slicedRequest = new bytes(request.length - 4);
+        for (uint256 i = 4; i < request.length; i++) {
+            slicedRequest[i - 4] = request[i];
+        }
+
+        AttestationRequest memory requestDecoded = abi.decode(
+            slicedRequest,
+            (AttestationRequest)
+        );
+
+        return attest(requestDecoded);
     }
 
     //-----------------------------
@@ -147,19 +146,10 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
     //-------------------------
     //DEFINE EXTERNAL FUNCTIONS
     //-------------------------
-    //change the gas amount
-    function changeGasAmount(uint256 _gasAmount) external {
-        gasAmount = _gasAmount;
-    }
 
-    /**
-     * @notice Returns the cost (in wei) of a greeting
-     */
-    function quoteCrossChainGreeting(
+    function quoteCrossChainAttestation(
         uint16 targetChain
     ) public view returns (uint256 cost) {
-        // Cost of requesting a message to be sent to
-        // chain 'targetChain' with a gasLimit of 'GAS_LIMIT'
         (cost, ) = wormholeRelayer.quoteEVMDeliveryPrice(
             targetChain,
             0,
@@ -167,50 +157,67 @@ contract MegaMaskAttest is IEAS, IWormholeReceiver {
         );
     }
 
-    /**
-     * @notice Updates the list of 'greetings'
-     * and emits a 'GreetingReceived' event with 'greeting'
-     * on the HelloWormhole contract at
-     * chain 'targetChain' and address 'targetAddress'
-     */
-    // function sendCrossChainGreeting(
-    //     uint16 targetChain,
-    //     address targetAddress,
-    //     string memory greeting
-    // ) public payable {
-    //     bytes memory payload = abi.encode(greeting, msg.sender);
-    //     uint256 cost = quoteCrossChainGreeting(targetChain);
-    //     require(msg.value == cost, "Incorrect payment");
-    //     wormholeRelayer.sendPayloadToEvm{value: cost}(
-    //         targetChain,
-    //         targetAddress,
-    //         payload,
-    //         0, // no receiver value needed
-    //         GAS_LIMIT
-    //     );
-    // }
-
-    //this is used to post attestation to Sepolia
-    function sendInterchainCall(
-        uint32 _destinationDomain,
-        bytes32 _recipientAddress,
-        bytes calldata _messageBody
+    function sendCrossChainAttestation(
+        uint16 targetChain,
+        address targetAddress,
+        bytes calldata request
     ) public {
-        _updateInstances(); // Ensure instances are up-to-date
-        bytes32 messageId = mailbox.dispatch(
-            _destinationDomain,
-            _recipientAddress,
-            _messageBody
+        uint256 cost = quoteCrossChainAttestation(targetChain);
+        wormholeRelayer.sendPayloadToEvm{value: cost}(
+            targetChain,
+            targetAddress,
+            abi.encode(request),
+            0, // no receiver value needed since we're just passing a message
+            GAS_LIMIT
+        );
+    }
+
+    function receiveWormholeMessages(
+        bytes memory payload,
+        bytes[] memory, // additionalVaas
+        bytes32, // address that called 'sendPayloadToEvm' (HelloWormhole contract address)
+        uint16 sourceChain,
+        bytes32 deliveryHash // this can be stored in a mapping deliveryHash => bool to prevent duplicate deliveries
+    ) public payable override {
+        require(msg.sender == address(wormholeRelayer), "Only relayer allowed");
+
+        // Ensure no duplicate deliveries
+        require(
+            !seenDeliveryVaaHashes[deliveryHash],
+            "Message already processed"
         );
 
-        // Get the required payment from the IGP.
-        uint256 quote = igp.quoteGasPayment(_destinationDomain, gasAmount);
+        //emit an event to say that the message is received
+        emit AttestationReceived(string(payload), sourceChain, msg.sender);
 
-        igp.payForGas{value: quote}(
-            messageId, // The ID of the message that was just dispatched
-            _destinationDomain, // The destination domain of the message
-            gasAmount, // 550k gas to use in the recipient's handle function
-            address(this) // refunds go to msg.sender, who paid the msg.value
+        // Mark the delivery as seen
+        seenDeliveryVaaHashes[deliveryHash] = true;
+
+        // Parse the payload and do the corresponding actions!
+        bytes memory slicedPayload = new bytes(payload.length - 4);
+        for (uint256 i = 4; i < payload.length; i++) {
+            slicedPayload[i - 4] = payload[i];
+        }
+
+        AttestationRequest memory request = abi.decode(
+            slicedPayload,
+            (AttestationRequest)
+        );
+
+        // Post the attestation to Sepolia
+        bytes32 attestationHash = eas.attest(request);
+
+        // Emit an event to say that the attestation is posted to Sepolia
+        emit AttestationPostedToSepolia(
+            msg.sender,
+            request.schema,
+            request.data.recipient,
+            request.data.expirationTime,
+            request.data.revocable,
+            request.data.refUID,
+            request.data.data,
+            request.data.value,
+            attestationHash
         );
     }
 
